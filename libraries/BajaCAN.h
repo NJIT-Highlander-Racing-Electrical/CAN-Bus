2c0212193896f9ec96027d62f117df448d1b9ff9
andrewsantella <72953033+andrewsantella@users.noreply.github.com>
2024-10-30 17:17:20 -0400

    Add second temperature sensor

--- libraries/BajaCAN.h
+++ libraries/BajaCAN.h
@@ -130,7 +130,8 @@ Subsystem currentSubsystem;
 // CVT Tachometer CAN IDs
 const int primaryRPM_ID = 0x01;
 const int secondaryRPM_ID = 0x02;
-const int cvtTemperature_ID = 0x03;
+const int cvtPrimaryTemperature_ID = 0x03;
+const int cvtSecondaryTemperature_ID = 0x04;
 
 // Wheel Speed Sensors CAN IDs
 const int frontLeftWheelSpeed_ID = 0x0B;
@@ -182,7 +183,8 @@ const int fuelPercentage_ID = 0x4C;
 // CVT Tachometer
 int primaryRPM;
 int secondaryRPM;
-int cvtTemperature;
+int cvtPrimaryTemperature;
+int cvtSecondaryTemperature;
 
 // Wheel Speed Sensors CAN
 int frontLeftWheelSpeed;
@@ -320,9 +322,14 @@ void CAN_Task_Code(void* pvParameters) {
         CAN.print(secondaryRPM);
         CAN.endPacket();
 
-        CAN.beginPacket(cvtTemperature_ID);
-        CAN.print(cvtTemperature);
+        CAN.beginPacket(cvtPrimaryTemperature_ID);
+        CAN.print(cvtPrimaryTemperature);
         CAN.endPacket();
+
+          CAN.beginPacket(cvtSecondaryTemperature_ID);
+        CAN.print(cvtSecondaryTemperature);
+        CAN.endPacket();
+          
         break;
 
     case WHEEL_SPEED:

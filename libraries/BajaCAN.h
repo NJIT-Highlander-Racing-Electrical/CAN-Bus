a77a981450e7fdb547ce83c1174e3603db5ec1e3
andrewsantella <72953033+andrewsantella@users.noreply.github.com>
2025-01-07 20:32:35 -0500

    Minor Updates to format

--- libraries/BajaCAN.h
+++ libraries/BajaCAN.h
@@ -1,6 +1,6 @@
 /*********************************************************************************
 *   
-*   BajaCAN.h  -- Version 1.2.11 
+*   BajaCAN.h  -- Version 1.2.12 
 * 
 *   The goal of this BajaCAN header/driver is to enable all subsystems throughout
 *   the vehicle to use the same variables, data types, and functions. That way,
@@ -786,7 +786,6 @@ void CAN_Task_Code(void* pvParameters) {
           CAN.endPacket();
           break;
       }
-
-}
+    }
   }
 }

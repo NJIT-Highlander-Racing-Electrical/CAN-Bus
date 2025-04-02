6dae44d8adb1a2977c5978207d73b7f819514f15
andrewsantella <72953033+andrewsantella@users.noreply.github.com>
2025-04-02 15:51:16 -0400

    Whoops forgot to update version number again

--- libraries/BajaCAN.h
+++ libraries/BajaCAN.h
@@ -1,6 +1,6 @@
 /*********************************************************************************
 *   
-*   BajaCAN.h  -- Version 1.3.3
+*   BajaCAN.h  -- Version 1.3.4
 * 
 *   The goal of this BajaCAN header/driver is to enable all subsystems throughout
 *   the vehicle to use the same variables, data types, and functions. That way,

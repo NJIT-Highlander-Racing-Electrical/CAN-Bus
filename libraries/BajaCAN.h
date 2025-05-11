81328cf18699758ae38588e3e5b097d0c9ef7464
andrewsantella <72953033+andrewsantella@users.noreply.github.com>
2025-05-10 20:24:55 -0400

    Update BajaCAN.h

--- libraries/BajaCAN.h
+++ libraries/BajaCAN.h
@@ -108,7 +108,7 @@
 #include "arduino-CAN/src/CAN.h"
 
 // Definitions for all CAN setup parameters
-#define CAN_BAUD_RATE 1000E3
+#define CAN_BAUD_RATE 500E3
 #define CAN_TX_GPIO 25
 #define CAN_RX_GPIO 26
 
